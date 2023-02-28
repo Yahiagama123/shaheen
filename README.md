8f9e22ad21c0f34a62ce539fb296e31f855ab1cc
yahia <yahia.vaml.g@gmial.com>
2023-02-28 17:28:59 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Shaheen
+
+alshaheen
+
+#### License
+
+MIT
---
+++ shaheen/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Shaheen",
+			"type": "module",
+			"label": _("Shaheen")
+		}
+	]
