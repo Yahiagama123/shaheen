3febe7486f0d0c3d033eb3f616f9530bbcdba13f
yahia <yahia.vaml.g@gmial.com>
2023-03-09 09:18:35 +0200

    3th commet

---
+++ shaheen/security/doctype/enter_statment/test_enter_statment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, yahia and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEnterStatment(FrappeTestCase):
+	pass
